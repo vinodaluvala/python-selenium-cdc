78b569ad9b517d7c2815f0d271da335eeee69af6
vinodaluvala <vinod.vinnu542@gmail.com>
2024-01-12 23:35:25 +0530

    dict to list and vice versa

--- interviewPrep/string_manipulations.py
+++ interviewPrep/string_manipulations.py
@@ -7,5 +7,20 @@ def string_mani():
             hm[i]+=1
         else:
             hm[i] = 1
-    print(hm.get())
+    print(hm)
 string_mani()
+
+# Conversion of two list into Dictionary Using Python
+
+def list_to_dict():
+    x = [1, 2, 3]
+    y = ["one", "Two", 'Three']
+    result = dict(zip(x, y))
+    print(result)
+list_to_dict()
+
+def dict_to_list():
+    x = {1: 'one', 2: 'Two', 3: 'Three'}
+    for i in x.items():
+        print(list(i))
+dict_to_list()
